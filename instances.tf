bffbe50ce7d5f47a561e794cd891010f9ab657df
sara galal <53039820+sera364@users.noreply.github.com>
2022-05-24 13:04:37 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "sara-public-instance"{
 }
 
 resource "aws_instance" "sara-private-instance"{
-  ami                         = "ami-0fb653ca2d3203ac1"
+  ami                         = "ami-0fa49cc9dc8d62c84"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.public_key_pair.id
   vpc_security_group_ids      = [aws_security_group.security-group-private.id]
