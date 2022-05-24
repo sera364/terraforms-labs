3b093c160f6c903811a13e190837879bb1cfd2d6
sara galal <53039820+sera364@users.noreply.github.com>
2022-05-24 13:57:15 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-jenkins-96"
+    bucket = "terraform-jenkins-97"
     key    = "sarastate.tf"
     region = "eu-central-2"
     profile = "default"
