8b135d9b017566eda4a38cf0570c7a1cc84aaf02
sara galal <53039820+sera364@users.noreply.github.com>
2022-06-03 20:06:44 +0200

    Create output-jen.tf

---
+++ output-jen.tf
@@ -0,0 +1,12 @@
+output puplicec2 {
+  value       = aws_instance.sara-public-instance.public_ip
+  
+}
+output privateec2 {
+  value       = aws_instance.sara-private-instance.private_ip
+  
+}
+output private_key {
+  value       = tls_private_key.private_key_pair.private_key_pem
+  
+}
