1c05a47630b347f3cafc13866cbac0e62e0993d6
sara galal <53039820+sera364@users.noreply.github.com>
2022-06-07 17:49:52 +0200

    Update output-jen.tf

--- output-jen.tf
+++ output-jen.tf
@@ -11,3 +11,26 @@ output private_key {
   sensitive = true
   
 }
+
+output rds_hostname {
+  value = aws_db_instance.rds-sara.address
+}
+
+output rds_port {
+  value = aws_db_instance.rds-sara.port
+}
+
+output rds_username {
+  value = aws_db_instance.rds-sara.username
+}
+
+output password {
+  value = aws_db_instance.rds-sara.password
+}
+
+output redis_hostname {
+  value = aws_elasticache_cluster.redis-sara.cache_nodes[0].address 
+}
+output redis_port {
+  value = aws_elasticache_cluster.redis-sara.port
+}
